ce198ea08ff2f7c49c38a6c5e4ec09e93dc768c7
冯垚杰 <yaojie.feng@symbio.com>
2023-02-08 16:35:54 +0800

    解决编译错误

--- Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
+++ Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
@@ -528,6 +528,11 @@ extension ChartDataSet: RandomAccessCollection {
 
 // MARK: RangeReplaceableCollection
 extension ChartDataSet: RangeReplaceableCollection {
+    
+    public func replaceSubrange<C>(_ subrange: Swift.Range<Index>, with newElements: C) where C : Collection, Element == C.Element {
+                fatalError()
+        }
+    
     public func append(_ newElement: Element) {
         calcMinMax(entry: newElement)
         entries.append(newElement)
