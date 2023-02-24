e5c88c4d7cfe1ad52708727d9cd2c2835adbdd67
冯垚杰 <yaojie.feng@symbio.com>
2023-02-24 17:21:18 +0800

    添加注释

--- Source/Charts/Interfaces/ChartDataProvider.swift
+++ Source/Charts/Interfaces/ChartDataProvider.swift
@@ -16,17 +16,22 @@ import CoreGraphics
 public protocol ChartDataProvider
 {
     /// The minimum x-value of the chart, regardless of zoom or translation.
+    /// 图表的最小x值，与缩放或平移无关。
     var chartXMin: Double { get }
     
     /// The maximum x-value of the chart, regardless of zoom or translation.
+    /// 图表的最大x值，与缩放或平移无关。
     var chartXMax: Double { get }
     
     /// The minimum y-value of the chart, regardless of zoom or translation.
+    /// 图表的最小y值，与缩放或平移无关。
     var chartYMin: Double { get }
     
     /// The maximum y-value of the chart, regardless of zoom or translation.
+    /// 图表的最大y值，不考虑缩放或平移。
     var chartYMax: Double { get }
     
+    /// 最大高亮显示距离
     var maxHighlightDistance: CGFloat { get }
     
     var xRange: Double { get }
